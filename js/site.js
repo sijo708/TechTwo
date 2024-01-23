f410ee67a735d841f3f2ef985b9e08949c1c5cab
sijo708 <sijo708@yahoo.com>
2024-01-23 17:07:34 -0500

    corrected

--- js/site.js
+++ js/site.js
@@ -8,32 +8,49 @@ function getValues() {
             title: 'Oops',
             text: 'Please enter at least 2 characters to check for palindrome.'
         });
-    } else {
-        // send the input to be checked if it is a palindrome
-        let isPalindrome = checkForPalindrome(userMessage);
-        // send results to be displayed
-        displayResults(isPalindrome);
+
+        return;
     }
+
+    // send the input to be checked if it is a palindrome
+    let isPalindrome = checkForPalindrome(userMessage);
+
+    // send results to be displayed
+    let reversedInput;
+    if (isPalindrome) {
+        reversedInput = 'is';
+    } else {
+        reversedInput = 'is not';
+    }
+
+    let stringObject = {
+        input: userMessage,
+        reversedInput: reversedInput
+    };
+
+    displayResults(stringObject);
 }
 
 function checkForPalindrome(userMessage) {
-    let string = userMessage.replace(/\s+/g, '').toLowerCase();
+    let string = userMessage.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     let reversedString = string.split('').reverse().join('');
 
     return string === reversedString;
 }
 
-function displayResults(isPalindrome) {
-    let resultMessage;
+function displayResults(stringObject) {
+    let alertBox = document.getElementById('alert');
+    alertBox.classList.remove('invisible');
 
-    if (isPalindrome) {
-        resultMessage = 'is';
+    if (stringObject.reversedInput === 'is') {
+        // Display success message
+        alertBox.classList.replace("alert-danger", "alert-success");
+        alertBox.innerHTML = `<div><h4>Well done! You entered a Palindrome!</h4>This is a palindrome! <span class="fw-bold" id="results"></span></div>`;
+        document.getElementById('results').textContent = stringObject.input;
     } else {
-        resultMessage = 'is not';
+        // Display error message
+        alertBox.classList.replace("alert-success", "alert-danger");
+        alertBox.innerHTML = `<div><h4>Oh no!</h4>This is not a palindrome! <span class="fw-bold" id="results"></span></div>`;
+        document.getElementById('results').textContent = stringObject.input;
     }
-
-    document.getElementById('result').innerHTML =
-        `The entered string ${resultMessage} a palindrome.`;
-
-    document.getElementById('alert').classList.remove('invisible');
 }
