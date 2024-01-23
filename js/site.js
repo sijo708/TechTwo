4ec3ccfd60a412f2cf6555e0f5788d86de80a935
sijo708 <sijo708@yahoo.com>
2024-01-23 17:10:12 -0500

    updated

--- js/site.js
+++ js/site.js
@@ -35,14 +35,14 @@ function checkForPalindrome(userMessage) {
     let string = userMessage.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     let reversedString = string.split('').reverse().join('');
 
-    return string === reversedString;
+    return string == reversedString;
 }
 
 function displayResults(stringObject) {
     let alertBox = document.getElementById('alert');
     alertBox.classList.remove('invisible');
 
-    if (stringObject.reversedInput === 'is') {
+    if (stringObject.reversedInput == 'is') {
         // Display success message
         alertBox.classList.replace("alert-danger", "alert-success");
         alertBox.innerHTML = `<div><h4>Well done! You entered a Palindrome!</h4>This is a palindrome! <span class="fw-bold" id="results"></span></div>`;
