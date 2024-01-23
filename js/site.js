c0ab23901cc47fa46aa6f50a55af975d835f9460
sijo708 <sijo708@yahoo.com>
2024-01-23 15:26:41 -0500

    init

--- js/site.js
+++ js/site.js
@@ -1,12 +1,39 @@
-function displayMessage(){
-    let inputBox = document.getElementById('message');
-    let message = inputBox.value;
+function getValues() {
+    let userMessage = document.getElementById('message').value;
 
-    Swal.fire(
-        {
+    if (userMessage.length < 2) {
+        Swal.fire({
+            icon: 'error',
             backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-    );
+            title: 'Oops',
+            text: 'Please enter at least 2 characters to check for palindrome.'
+        });
+    } else {
+        // send the input to be checked if it is a palindrome
+        let isPalindrome = checkForPalindrome(userMessage);
+        // send results to be displayed
+        displayResults(isPalindrome);
+    }
+}
+
+function checkForPalindrome(userMessage) {
+    let string = userMessage.replace(/\s+/g, '').toLowerCase();
+    let reversedString = string.split('').reverse().join('');
+
+    return string === reversedString;
+}
+
+function displayResults(isPalindrome) {
+    let resultMessage;
+
+    if (isPalindrome) {
+        resultMessage = 'is';
+    } else {
+        resultMessage = 'is not';
+    }
+
+    document.getElementById('result').innerHTML =
+        `The entered string ${resultMessage} a palindrome.`;
+
+    document.getElementById('alert').classList.remove('invisible');
 }
