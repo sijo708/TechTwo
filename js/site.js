e79d82fcc7ff274e9162d78fc2d69ee42ce3159b
sijo708 <sijo708@yahoo.com>
2024-01-24 14:59:55 -0500

    update3

--- js/site.js
+++ js/site.js
@@ -10,47 +10,59 @@ function getValues() {
         });
 
         return;
-    }
-
-    // send the input to be checked if it is a palindrome
-    let isPalindrome = checkForPalindrome(userMessage);
-
-    // send results to be displayed
-    let reversedInput;
-    if (isPalindrome) {
-        reversedInput = 'is';
     } else {
-        reversedInput = 'is not';
+        let isPalindrome = checkForPalindrome(userMessage);
+        let userMessageReversed = reverseString(userMessage);
+
+        let results = {
+            input: userMessage,
+            reversed: userMessageReversed,
+            isPalindrome: isPalindrome
+        };
+
+        displayResults(results);
     }
-
-    let stringObject = {
-        input: userMessage,
-        reversedInput: reversedInput
-    };
-
-    displayResults(stringObject);
 }
 
-function checkForPalindrome(userMessage) {
-    let string = userMessage.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-    let reversedString = string.split('').reverse().join('');
+function displayResults(results) {
+    let alert = document.getElementById('alert');
+    alert.classList.remove('invisible', 'alert-success', 'alert-danger');
 
-    return string == reversedString;
+    if (results.isPalindrome == true) {
+        // make the alert green
+        alert.classList.add('alert-success');
+        // make the header say success
+        alert.querySelector('h4').innerHTML = 'Well Done! You entered a palindrome';
+    } else {
+        alert.classList.add('alert-danger');
+        alert.querySelector('h4').innerHTML = 'Oh No! That is not a palindrome';
+    }
+
+    // display the input & reverse in the body
+    alert.querySelector('p').textContent = `You entered: ${results.input}. Your message reversed is: ${results.reversed}`;
 }
 
-function displayResults(stringObject) {
-    let alertBox = document.getElementById('alert');
-    alertBox.classList.remove('invisible');
+function checkForPalindrome(input) {
+    // take the user input and reverse it
+    let lowerCaseInput = input.toLowerCase();
+    lowerCaseInput = lowerCaseInput.replace(/[^a-z0-9]/gi, '');
 
-    if (stringObject.reversedInput == 'is') {
-        // Display success message
-        alertBox.classList.replace("alert-danger", "alert-success");
-        alertBox.innerHTML = `<div><h4>Well done! You entered a Palindrome!</h4>This is a palindrome! <span class="fw-bold" id="results"></span></div>`;
-        document.getElementById('results').textContent = stringObject.input;
-    } else {
-        // Display error message
-        alertBox.classList.replace("alert-success", "alert-danger");
-        alertBox.innerHTML = `<div><h4>Oh no!</h4>This is not a palindrome! <span class="fw-bold" id="results"></span></div>`;
-        document.getElementById('results').textContent = stringObject.input;
-    }    
+    let reversed = reverseString(lowerCaseInput);
+
+    // see if the reversed string is the same as the input
+    let isPalindrome = reversed == lowerCaseInput;
+
+    // return a value indicating whether it is or is not a palindrome
+    return isPalindrome;
+}
+
+function reverseString(inputString) {
+    let result = '';
+
+    // loop through the string from the end towards the beginning
+    for (let index = inputString.length - 1; index >= 0; index--) {
+        result += inputString[index];
+    }
+
+    return result;
 }
